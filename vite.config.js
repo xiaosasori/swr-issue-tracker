49b7c7472f4cfb665d2f4e36cf2a9ab69eed9315
Nguyen Minh Anh <xiaosasori@gmail.com>
2022-11-09 09:15:19 +0900

    chore: linting

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
